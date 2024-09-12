452df01eed7c65dbbd6d29c7394b44eedc45b685
Shahid Iqbal <m.shahid9115@gmail.com>
2024-09-12 13:54:10 +0500

    UpLoad Figma Desgin Template

---
+++ VerokeTask11Sep24/main.js
@@ -0,0 +1,36 @@
+
+
+function ModalShow (){
+    let modal = document.getElementById("modal-Section");
+    if (modal.style.display="none") {
+        modal.style.display="block"
+        modal.style.justifyContent="center"
+        modal.style.alignItems="center"
+    }
+}
+
+function MenuShow (){
+    let togal= document.getElementById("navBar");
+    let close = document.getElementById("closeNavBar");
+    if (togal.style.display="none") {
+        togal.style.display="grid"
+        close.style.display="flex"
+    }
+}
+
+function MenuHide (){
+    let togal= document.getElementById("navBar");
+    let close = document.getElementById("closeNavBar");
+    if (togal.style.display="flex") {
+        togal.style.display="none"
+        close.style.display="none"
+        window.location.reload();
+    }
+}
+
+function ModalHide (){
+    let modal = document.getElementById("modal-Section");
+    if (modal.style.display="block") {
+        modal.style.display="none"
+    }
+}

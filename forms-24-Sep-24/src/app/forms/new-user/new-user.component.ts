c43c1a63e9eb2bcf117370b1561599dc664ebd23
Shahid Iqbal <m.shahid9115@gmail.com>
2024-09-26 17:50:23 +0500

    update angular Form

--- forms-24-Sep-24/src/app/forms/new-user/new-user.component.ts
+++ forms-24-Sep-24/src/app/forms/new-user/new-user.component.ts
@@ -1,60 +1,60 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ReactiveFormsModule,FormControl, FormGroup , Validators} from '@angular/forms';
+import { ReactiveFormsModule, FormControl, FormGroup, Validators } from '@angular/forms';
 import { Output, EventEmitter } from '@angular/core';
 import { Input } from '@angular/core';
 
 @Component({
   selector: 'app-new-user',
   standalone: true,
-  imports: [ReactiveFormsModule,CommonModule],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './new-user.component.html',
   styleUrl: './new-user.component.css'
 })
 export class NewUserComponent {
-  @Input() eitdToData:any;
-  @Output() buttonClick = new EventEmitter<any>();  
+  @Input() eitdToData: any;
+  @Output() buttonClick = new EventEmitter<any>();
 
-  id:number=1;
+  id: number = 1;
 
   registerationForm = new FormGroup({
     id: new FormControl(),
-    name: new FormControl('',[Validators.required]),
-    email: new FormControl('',Validators.required),
-    phone: new FormControl('',[Validators.required]),
-    address: new FormControl('',[Validators.required]),
-    education: new FormControl('',[Validators.required]),
+    name: new FormControl('', [Validators.required]),
+    email: new FormControl('', Validators.required),
+    phone: new FormControl('', [Validators.required]),
+    address: new FormControl('', [Validators.required]),
+    education: new FormControl('', [Validators.required]),
   });
   onSubmit() {
     if (this.registerationForm.invalid) {
-      alert("Fill the Form");
+      alert("Fill Form");
     }
-    else{
-      this.registerationForm.value.id=this.id;
-      this.id=this.id+1;
+    else {
+      this.registerationForm.value.id = this.id;
+      this.id = this.id + 1;
       this.buttonClick.emit(this.registerationForm.value);
       this.registerationForm.reset();
     }
-  } 
+  }
 
-  disAble:boolean=true;
-  updateButton:boolean=false;
-  clearButton:boolean=false;
-  
-  update(){
+  disAble: boolean = true;
+  updateButton: boolean = false;
+  clearButton: boolean = false;
+
+  update() {
     this.buttonClick.emit(this.registerationForm.value);
     this.registerationForm.reset();
-    this.updateButton=false;
-    this.disAble=true;
-    this.clearButton=false;
+    this.updateButton = false;
+    this.disAble = true;
+    this.clearButton = false;
   }
- 
-  clear(){
+
+  clear() {
     this.registerationForm.reset();
-    this.disAble=true;
-    this.clearButton=false;
+    this.disAble = true;
+    this.clearButton = false;
   }
-  
+
   // Patch(){
   //   this.updateButton=true;
   //   this.registerationForm.patchValue(this.eitdToData.objToView);
@@ -65,25 +65,25 @@ export class NewUserComponent {
   //   this.disAble=false;
   // }
 
-  ngOnChanges(){
+  ngOnChanges() {
     // console.log(this.eitdToData.type);
-    if (this.eitdToData.type=="View") {
-      this.updateButton=false;
-      this.clearButton=true;
-      this.disAble=false;
+    if (this.eitdToData.type == "View") {
+      this.updateButton = false;
+      this.clearButton = true;
+      this.disAble = false;
       this.registerationForm.patchValue(this.eitdToData.objToView);
     }
-    if (this.eitdToData.type=="Edit") {
-      this.updateButton=true;
-      this.clearButton=false;
-      this.disAble=false;
+    if (this.eitdToData.type == "Edit") {
+      this.updateButton = true;
+      this.clearButton = false;
+      this.disAble = false;
       this.registerationForm.patchValue(this.eitdToData.objToView);
     }
   }
-  ngOnInit(){
-    this.disAble=true;
-    this.updateButton=false;
-    this.clearButton=false;
+  ngOnInit() {
+    this.disAble = true;
+    this.updateButton = false;
+    this.clearButton = false;
   }
-  
+
 }
--- forms-24-Sep-24/src/app/forms/table/table.component.ts
+++ forms-24-Sep-24/src/app/forms/table/table.component.ts
@@ -6,46 +6,46 @@ import { Output, EventEmitter } from '@angular/core';
 @Component({
   selector: 'app-table',
   standalone: true,
-  imports: [CommonModule,FormsModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './table.component.html',
   styleUrl: './table.component.css'
 })
 export class TableComponent {
-  @Input() getData:any;
-  @Input() actions:any;
-  @Input() dataForHeading:any;
-  @Input() actionsHeading:any;
-  @Output() toSent = new EventEmitter<any>(); 
+  @Input() getData: any;
+  @Input() actions: any;
+  @Input() dataForHeading: any;
+  @Input() actionsHeading: any;
+  @Output() toSent = new EventEmitter<any>();
 
-  Delete(itemId:any){
-    this.getData.forEach((e:any,index:number) => {
-      if (e.id==itemId) {
-        this.getData.splice(index, 1); 
-      }
-    });
-    alert("Delete");
+  Delete(itemId: any) {
+    let userResponse = confirm("Do You Want to Delete This");
+    if (userResponse) {
+      this.getData.forEach((e: any, index: number) => {
+        if (e.id == itemId) {
+          this.getData.splice(index, 1);
+        }
+      });
+    }
   }
 
-  View(itemId:any){
-    const objToView = this.getData.find((obj:any) => obj.id === itemId);  
-    this.toSent.emit({objToView,type:'View'});
-    // alert("info is send for View! Please Press Patch Button");
+  View(itemId: any) {
+    const objToView = this.getData.find((obj: any) => obj.id === itemId);
+    this.toSent.emit({ objToView, type: 'View' });
   }
 
-  Edit(itemId:any){
-    const objToView = this.getData.find((obj:any) => obj.id === itemId);  
-    this.toSent.emit({objToView,type:'Edit'});
-  //  alert("info is send for eitd! Please Press Patch Button");
+  Edit(itemId: any) {
+    const objToView = this.getData.find((obj: any) => obj.id === itemId);
+    this.toSent.emit({ objToView, type: 'Edit' });
   }
 
-  call(id:any,lable:any){
-    if (lable=="View") {
+  call(id: any, lable: any) {
+    if (lable == "View") {
       this.View(id);
     }
-    if (lable=="Delete") {
-       this.Delete(id);
+    if (lable == "Delete") {
+      this.Delete(id);
     }
-    if (lable=="Edit") {
+    if (lable == "Edit") {
       this.Edit(id);
     }
   }
